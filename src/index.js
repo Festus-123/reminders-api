633d0432f5ca7d04d61472d019066faa4fcf0bbf
Festus Phillip <st.festus4cruise@gmail.com>
2026-09-04 15:04:46 +0100

    fix: bind server to 0.0.0.0 for external accessibility

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import app from "./app/app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Reminder Api working on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Reminder Api working on port ${PORT}`));
